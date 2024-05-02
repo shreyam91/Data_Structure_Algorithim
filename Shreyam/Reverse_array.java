00d0440830672213f4c5a0540acd90f8b0ecc46d
shreyam <shreyam91183@gmail.com>
2024-05-02 23:37:08 +0530

    reverse array

---
+++ Shreyam/Reverse_array.java
@@ -0,0 +1,12 @@
+package Shreyam;
+
+public class Reverse_array {
+    public static void main(String[] args) {
+        System.out.println("Reverse Array");
+        int[] arr = {1,2,3,4,5};
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
