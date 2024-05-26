4a6f05b92cd5cf4910ebfb04682de30e74552604
shreyam <shreyam91183@gmail.com>
2024-05-26 23:05:38 +0530

    Union Number in sorted array

---
+++ Shreyam/intersectionNumber.java
@@ -0,0 +1,32 @@
+package Shreyam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class intersectionNumber {
+    
+    public static void main(String[] args) {
+        int arr1[] ={1,2,3,4,4,5};
+        int arr2[] ={4,5,6,7,7,8};
+
+        int left=0;
+        int right=0;
+
+        List<Integer> res = new ArrayList<>();
+
+        while(left<arr1.length || right<arr2.length){
+
+            //duplicate number 
+            while(left>0 &&left<arr1.length && arr1[left]==arr1[left-1]){
+                left++;
+            }
+            while(right>0 &&right<arr2.length && arr2[right]==arr2[right-1]){
+                right++;
+            }
+
+            //one array exhaust 
+
+            //comparison 
+        }
+    }
+}
