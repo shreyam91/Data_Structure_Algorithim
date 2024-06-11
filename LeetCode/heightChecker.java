18617ffe2d936050bd34ae49a817fbfeff0d1c22
shreyam <shreyam91183@gmail.com>
2024-06-11 11:31:29 +0530

    Relative Sort Array

--- LeetCode/heightChecker.java
+++ LeetCode/heightChecker.java
@@ -46,23 +46,4 @@ public class heightChecker {
 // Indices 2, 4, and 5 do not match.
 // Example 2:
 
-// Input: heights = [5,1,2,3,4]
-// Output: 5
-// Explanation:
-// heights:  [5,1,2,3,4]
-// expected: [1,2,3,4,5]
-// All indices do not match.
-// Example 3:
 
-// Input: heights = [1,2,3,4,5]
-// Output: 0
-// Explanation:
-// heights:  [1,2,3,4,5]
-// expected: [1,2,3,4,5]
-// All indices match.
- 
-
-// Constraints:
-
-// 1 <= heights.length <= 100
-// 1 <= heights[i] <= 100
