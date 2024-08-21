a5920d486b15e4379673f0469e2f6c6544465627
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-08-21 22:04:05 +0530

    number pattern #1

---
+++ Java/numberPattern.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class numberPattern {
+    public static void main(String[] args) {
+        int n=5,m=1;
+        
+        for(int i=1; i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(m +  " ");
+                m++;
+            }
+            System.out.println();
+    }
+}
+}
