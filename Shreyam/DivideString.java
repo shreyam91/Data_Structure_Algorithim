ae0521328876a9bf14299d0ad6088e719579f26e
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-08-28 21:16:35 +0530

    blank

---
+++ Shreyam/DivideString.java
@@ -0,0 +1,15 @@
+package Shreyam;
+
+public class DivideString {
+    public static void main(String[] args) {
+        String str = new String ("Adiyogi");
+        int n=2;
+
+        
+        String str2 = new String ();
+        str2.split("_");
+
+       System.out.println(str2);
+
+    }
+}
--- Shreyam/checkCharacter.java
+++ Shreyam/checkCharacter.java
@@ -14,6 +14,7 @@ public class checkCharacter {
         String st = "hello world!";
         int vowel=0;
         int consonant=0;
+        int spacialChar =0;
         // char ch ='';
 
         for(int i=0;i<st.length();i++){
@@ -25,6 +26,9 @@ public class checkCharacter {
                 // System.out.println(ch + " is vowel");
                 vowel++;
             }
+            else if (ch == ' '||ch=='!'){
+                spacialChar++;
+            }
             else{
                 // System.out.println(ch + " is consonant");
                 consonant++;
@@ -35,5 +39,6 @@ public class checkCharacter {
         // System.out.println("string to character " + ch);   
         System.out.println("vowel "+vowel);
         System.out.println("consonant "+consonant); 
+        System.out.println("special character "+spacialChar);
     }
 }
