4a4cedc7fdaf47fcf4eded697b79de5b663eebc2
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-09-04 22:21:39 +0530

    4 sept

--- Shreyam/Palindrome.java
+++ Shreyam/Palindrome.java
@@ -11,5 +11,16 @@ public class Palindrome {
         }
         System.out.println(s2);
         System.out.println("is palindrome or not: " + s1.equals(s2));
+
+        // boolean result = true;
+        // int length = s1.length();
+
+        // for(int i=0;i<length/2;i++){
+        //     if(s1.charAt(i) != s1.charAt(length-i-1)){
+        //         result = false;
+        //         break;
+        //     }
+        // }
+        // return result;
     }
 }
