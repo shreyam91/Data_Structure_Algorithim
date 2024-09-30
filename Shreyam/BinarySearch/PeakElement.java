e1e7922c748ec9e52efb990892f2a86cc9ffc8df
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-09-30 21:57:40 +0530

    Peak element of array

---
+++ Shreyam/BinarySearch/PeakElement.java
@@ -0,0 +1,37 @@
+package Shreyam.BinarySearch;
+
+public class PeakElement {
+
+    public static int binarySearch(int [] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start +(end - start)/2;
+
+            if ((mid == start || arr[mid] > arr[mid - 1]) && (mid == end || arr[mid] > arr[mid + 1])) {
+                return mid; 
+            }
+
+            if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                end = mid - 1;
+            } 
+            else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,10,20,15};
+
+        int result = binarySearch(arr);
+
+        if(result == -1)
+            System.out.println("no element found");
+        else
+            System.out.println("element found " + result + " " + arr[result]);
+    }
+    
+}
--- Shreyam/String/Palindrome.java
+++ Shreyam/String/Palindrome.java
@@ -2,15 +2,15 @@ package Shreyam.String;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String s1 = "madam";
-        String s2 = "";
+        // String s1 = "rahul";
+        // String s2 = "";
 
-        for(int i=0; i<s1.length();i++){
-            char ch = s1.charAt(i);
-            s2 = s2+ ch;
-        }
-        System.out.println(s2);
-        System.out.println("is palindrome or not: " + s1.equals(s2));
+        // for(int i=0; i<s1.length();i--){
+        //     char ch = s1.charAt(i);
+        //     s2 = s2+ ch;
+        // }
+        // System.out.println(s2);
+        // System.out.println("is palindrome or not: " + s1.equals(s2));
 
         // boolean result = true;
         // int length = s1.length();
@@ -22,5 +22,28 @@ public class Palindrome {
         //     }
         // }
         // return result;
+
+        // String str = "naman";
+        // StringBuilder str1 = new StringBuilder(str);
+
+        // // str1.reverse();
+        // if(str.equals(str1.reverse().toString())){
+        //     System.out.println("palinfrome");
+        // }else{
+        //     System.out.println("not");
+        // }
+
+
+        String str = "Madam";
+        StringBuilder str1 = new StringBuilder(str);
+
+        if(str.equalsIgnoreCase(str1.reverse().toString()))
+        { 
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("Not a palindrome");
+            }
+
     }
 }
