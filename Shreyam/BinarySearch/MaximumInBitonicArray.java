c48d122915c1751d8801774c11a735a4d0085d09
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-10-02 13:41:06 +0530

    Maximum element in Bitonic Array...

--- Shreyam/BinarySearch/MaximumInBitonicArray.java
+++ Shreyam/BinarySearch/MaximumInBitonicArray.java
@@ -1,13 +1,11 @@
 package Shreyam.BinarySearch;
 
-public class PeakElement {
-
-    public static int binarySearch(int [] arr){
-        int start = 0;
-        int end = arr.length-1;
+public class MaximumInBitonicArray {
+    public static int binarySearch(int[] arr){
+        int start =0, end = arr.length-1;
 
         while(start <= end){
-            int mid = start +(end - start)/2;
+            int mid = start + (end - start)/2;
 
             if ((mid == start || arr[mid] > arr[mid - 1]) && (mid == end || arr[mid] > arr[mid + 1])) {
                 return mid; 
@@ -20,18 +18,17 @@ public class PeakElement {
                 start = mid + 1;
             }
         }
-        return -1;
 
+        return -1;
     }
     public static void main(String[] args) {
-        int[] arr = {5,10,20,15,40,25};
+        int[] arr = {5,10,15,20,10,3};
 
         int result = binarySearch(arr);
 
         if(result == -1)
-            System.out.println("no element found");
+            System.out.println("No element found");
         else
-            System.out.println("element found " + result + " " + arr[result]);
+            System.out.println("Element found" + result + " " + arr[result]);
     }
-    
 }
