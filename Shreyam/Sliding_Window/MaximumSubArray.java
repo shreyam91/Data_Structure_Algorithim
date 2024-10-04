fb78929d48f9584f25a9b8e8c0c6204a2de2acb3
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-10-04 07:11:57 +0530

    Create MaximumSubArray.java

---
+++ Shreyam/Sliding_Window/MaximumSubArray.java
@@ -0,0 +1,30 @@
+package Shreyam.Sliding_Window;
+
+public class MaximumSubArray {
+    public static int slidingWindow(int[]arr, int k){
+        int len = arr.length;
+        int max = Integer.MIN_VALUE;
+        int i=0,j=1;
+        int sum =0;
+
+        for(i=0; i<k; i++){
+            sum = sum + arr[i];
+        }
+        max = sum;
+
+        for(i=k;i<len;i++){
+            sum += arr[i] - arr[i-k];
+            max = Math.max(max,sum); 
+        }
+
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,1,3,6,3};
+        int k = 3;
+
+        int result = slidingWindow(arr, k);
+        System.out.println("maximum sum " + k + "is: " + result);
+
+    }
+}
