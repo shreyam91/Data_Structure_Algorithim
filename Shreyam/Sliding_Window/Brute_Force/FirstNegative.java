351ccc90469fcc15cfd87d0e01d205387e7e650f
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-10-07 13:31:51 +0530

    1st negative number in every k subarray....

---
+++ Shreyam/Sliding_Window/Brute_Force/FirstNegative.java
@@ -0,0 +1,24 @@
+package Shreyam.Sliding_Window.Brute_Force;
+
+
+public class FirstNegative {
+    public static void slidingWindow(int[]arr, int k){
+        for(int i=0; i<= arr.length-k;i++){
+            int firstNegative = 0;
+            for(int j=0; j<k;j++){
+                if(arr[i+j] <0){
+                    firstNegative = arr[i+j];
+                    break;
+                }
+            }
+            System.out.print(firstNegative + " "); 
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,-1,5,6,-2,9,8,-3};
+        int k = 3;
+
+        slidingWindow(arr, k);
+    }
+}
