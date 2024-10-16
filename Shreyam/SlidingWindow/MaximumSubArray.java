42db861969a8fb240b9b73e33dd46398b93ea9b2
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-10-16 15:26:40 +0530

    ..

--- Shreyam/SlidingWindow/MaximumSubArray.java
+++ Shreyam/SlidingWindow/MaximumSubArray.java
@@ -24,7 +24,7 @@ public class MaximumSubArray {
         int k = 3;
 
         int result = slidingWindow(arr, k);
-        System.out.println("maximum sum " + k + "is: " + result);
+        System.out.println("maximum sum of window " + k + " is: " + result);
 
     }
 }
