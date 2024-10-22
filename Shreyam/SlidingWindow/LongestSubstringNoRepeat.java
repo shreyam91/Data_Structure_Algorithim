44a8374c7f3fa375d6ac54c1e844302ef614a0c2
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-10-22 13:53:20 +0530

    Longest SubString with unique character without K

---
+++ Shreyam/SlidingWindow/LongestSubstringNoRepeat.java
@@ -0,0 +1,47 @@
+// Longest substring with No Repeating Character
+
+package Shreyam.SlidingWindow;
+
+import java.util.HashMap;
+
+public class LongestSubstringNoRepeat {
+    public static Result noRepeatCharacter(String s){
+        HashMap<Character,Integer> charIndexMap = new HashMap<>();
+        int maxLength =0, leftPointer=0,startIndex=0;
+
+        for(int rightPointer =0;rightPointer<s.length();rightPointer++){
+            char currentChar = s.charAt(rightPointer);
+
+            if(charIndexMap.containsKey(currentChar)){
+                leftPointer = Math.max(charIndexMap.get(currentChar) +1, leftPointer);
+            }
+
+            charIndexMap.put(currentChar, rightPointer);
+
+            if(rightPointer - leftPointer +1 >maxLength){
+                maxLength = rightPointer - leftPointer +1;
+                startIndex = leftPointer;
+            }
+        }
+        String longestSubString = s.substring(startIndex,startIndex + maxLength);
+
+        return new Result(maxLength,longestSubString);
+    }
+
+    static class Result{
+        int length;
+        String subString;
+
+        Result(int length,String subString){
+            this.length = length;
+            this.subString = subString;
+
+        }
+    }
+    public static void main(String[] args) {
+        String s = "pwwrew";
+        Result res = noRepeatCharacter(s);
+        System.out.println("Character which makes long subString is: " + res.length + ":  " + res.subString);
+    }
+    
+}
