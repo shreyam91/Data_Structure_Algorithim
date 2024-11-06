c8f5029aadf8c01fc6b72f51791f17aadf4a9b33
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-11-06 16:08:16 +0530

    Nearest Smaller to right or next smaller element..

--- Shreyam/Stack/NextSmallerElement.java
+++ Shreyam/Stack/NextSmallerElement.java
@@ -2,14 +2,14 @@ package Shreyam.Stack;
 
 import java.util.Stack;
 
-public class NearestGreaterToLeft {
-    public static int[] findPreviousGreater(int[] arr){
+public class NextSmallerElement {
+    public static int[] nextSmallerElement(int arr[]){
         int n = arr.length;
         int result[] = new int[n];
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer>stack = new Stack<>();
 
-        for(int i= 0;i<n;i++){
-            while(!stack.isEmpty() && stack.peek() <= arr[i]){
+        for(int i=0; i<n ;i++){
+            while(!stack.isEmpty() && stack.peek() >= arr[i]){
                 stack.pop();
             }
             if(!stack.isEmpty()){
@@ -18,16 +18,15 @@ public class NearestGreaterToLeft {
                 result[i] = -1;
             }
             stack.push(arr[i]);
-        }
-
+            }
+        
         return result;
     }
-
     public static void main(String[] args) {
-        int[] arr = {1,3,2,4};
-        int [] result = findPreviousGreater(arr);
+        int[] arr = {4,5,2,10,8};
+        int result[] = nextSmallerElement(arr);
 
-        for(int i : result){
+        for(int i:result){
             System.out.print(i + " ");
         }
     }
