d30947cb76a7fa586aa3871dd9d13cedf131ba59
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-11-18 13:18:30 +0530

    Min element with O(1) space...

---
+++ Shreyam/Stack/MinElementStack.java
@@ -0,0 +1,48 @@
+package Shreyam.Stack;
+
+import java.util.Stack;
+
+public class MinElementStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+        public MinElementStack(){
+            stack = new Stack<>();
+            minStack = new Stack <>();
+        }
+    
+        public void push(int x){
+            stack.push(x);
+            if(minStack.isEmpty() || x<= minStack.peek()){
+                minStack.push(x);
+            }
+        }
+
+        public void pop(){
+            if(stack.isEmpty()){
+                return;
+            }
+            int poppedElement = stack.pop();
+            if(poppedElement == minStack.peek()){
+                minStack.pop();
+            }
+        }
+
+        public int getMin(){
+            if(minStack.isEmpty()){
+                throw new IllegalStateException("stack is Empty");
+            }
+            return minStack.peek();
+        }
+    public static void main(String[] args) {
+        int[] arr = {5, 15, 12, 19, 21, 3, 9};
+        
+        MinElementStack minStack = new MinElementStack();
+        
+        System.out.println("Pushing elements to the stack:");
+        for (int num : arr) {
+            minStack.push(num);
+            System.out.println("Pushed: " + num + ", Current Minimum: " + minStack.getMin());
+        }
+    }
+}
