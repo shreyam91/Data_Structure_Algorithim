e11e2a78507e972548aaedf2466f43034687e94a
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-12-18 13:06:57 +0530

    Fibonacci using recursion...

--- Shreyam/Recursion/Fibonacci.java
+++ Shreyam/Recursion/Fibonacci.java
@@ -1,12 +1,38 @@
 package Shreyam.Recursion;
 
+// public class Fibonacci {
+//     public static int fibonacci(int n){
+//         if( n<=1) return n;
+//         return fibonacci(n-1) + fibonacci(n-2);
+//     }
+//     public static void main(String[] args) {
+//         int n =10;
+//         System.out.println("Fibonacci of" + n + "is:" + fibonacci(n));
+//     }
+// }
+
+
+import java.util.HashMap;
+
 public class Fibonacci {
-    public static int fibonacci(int n){
-        if( n<=1) return n;
-        return fibonacci(n-1) + fibonacci(n-2);
+    
+    private static HashMap<Integer, Integer> memo = new HashMap<>();
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        
+        int result = fibonacci(n - 1) + fibonacci(n - 2);
+        memo.put(n, result);
+        
+        return result;
     }
+    
     public static void main(String[] args) {
-        int n =10;
-        System.out.println("Fibonacci of" + n + "is:" + fibonacci(n));
+        int n = 10;
+        System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
     }
 }
