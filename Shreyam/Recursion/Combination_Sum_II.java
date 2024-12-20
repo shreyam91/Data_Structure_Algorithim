2fb00b75089aa3423c2ce8060fc05d55753e334b
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2024-12-20 19:16:39 +0530

    Combination Sum 2

---
+++ Shreyam/Recursion/Combination_Sum_II.java
@@ -0,0 +1,31 @@
+package Shreyam.Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Combination_Sum_II {
+    private static void findCombination(int index, int[] arr, int target,List<List<Integer>>ans, List<Integer>result){
+        if(target == 0){
+            ans.add(new ArrayList<>(result));
+            return;
+        }
+        for(int i= index; i<arr.length;i++){
+            if(i>index && arr[i] == arr[i-1]) continue;
+            if(arr[i] > target) break;
+
+            result.add(arr[i]);
+            findCombination(i+1, arr, target-arr[i], ans, result);
+            result.remove(result.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        int [] num = {1,2,4,6,3,8,10};
+        int target = 10;
+        Arrays.sort(num);
+        List<List<Integer>> result = new ArrayList<>();
+        findCombination(0, num, target, result, new ArrayList<>());
+        System.out.println("Combinations that sum to " + target + ": " + result);
+
+    }
+}
