acb3129c260f15c5e151e97d21aed94b90a35662
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-01-08 20:57:46 +0530

    Count Prefix and Suffix Pairs I (LeetCode) Day-8

---
+++ Shreyam/Leetcode/Question3042.java
@@ -0,0 +1,41 @@
+// Question:- Count Prefix and Suffix Pairs I... 
+// You are given a 0-indexed string array words.
+// Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+// isPrefixAndSuffix(str1, str2) returns true if str1 is both a prefix and a suffix of str2, and false otherwise.
+// For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+
+// Return an integer denoting the number of index pairs (i, j) such that i < j, and isPrefixAndSuffix(words[i], words[j]) is true.
+
+// Example 1:
+// Input: words = ["a","aba","ababa","aa"]
+// Output: 4
+// Explanation: In this example, the counted index pairs are:
+// i = 0 and j = 1 because isPrefixAndSuffix("a", "aba") is true.
+// i = 0 and j = 2 because isPrefixAndSuffix("a", "ababa") is true.
+// i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true.
+// i = 1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true.
+// Therefore, the answer is 4.
+
+package Shreyam.Leetcode;
+
+public class Question3042 {
+    public static int countPrefixSuffixPairs(String[] words) {
+        int ans = 0;
+        int n = words.length;
+        for (int i = 0; i < n; ++i) {
+            String s = words[i];
+            for (int j = i + 1; j < n; ++j) {
+                String t = words[j];
+                if (t.startsWith(s) && t.endsWith(s)) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        String [] words = {"a","aba","ababa","aa"};
+        int result = countPrefixSuffixPairs(words);
+        System.out.println("There are " + result + " pairs possible.");
+    }
+}
