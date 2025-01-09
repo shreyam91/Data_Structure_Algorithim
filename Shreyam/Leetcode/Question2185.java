245b84365445e3fd227310bfbb879737a3a531e6
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-01-09 12:39:14 +0530

    Counting words with a given prefix(LeetCode) Day-9

---
+++ Shreyam/Leetcode/Question2185.java
@@ -0,0 +1,50 @@
+// Question:- Counting words with a given prefix.. 
+
+// You are given an array of strings words and a string pref.
+// Return the number of strings in words that contain pref as a prefix.
+// A prefix of a string s is any leading contiguous substring of s.
+
+// Example 1:
+
+// Input: words = ["pay","attention","practice","attend"], pref = "at"
+// Output: 2
+// Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+
+package Shreyam.Leetcode;
+
+public class Question2185 {
+    public static int prefixCount (String[] words,String prefix){
+        // <!-- Brute force approach --> 
+        // int length = words.length;
+        // int count = 0;
+        // for(int i=0;i<length;i++){
+        //     if(words[i].startsWith(prefix)){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        // <!--- Optimal Approach or Approach 2   (Both approach takes same time )--->
+
+        int count = 0;
+        for (String word : words) {
+            boolean isPrefix = true;
+            for (int i = 0; i < prefix.length(); ++i) {
+                if (i >= word.length() || word.charAt(i) != prefix.charAt(i)) {
+                    isPrefix = false;
+                    break;
+                }
+            }
+            if (isPrefix) {
+                count++;
+            }
+        }
+        return count;
+    } 
+    public static void main(String[] args) {
+        String [] words = {"pay","attention","practice","attend"};
+        String prefix = "at";
+        int result = prefixCount(words, prefix);
+        System.out.println("There are " + result + " words which are start with prefix :- " + prefix );
+    }
+}
