1ac7f275f0b9428af66e7869d699b45b3875ac9f
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-01-11 12:50:35 +0530

    Construct K Palindrome Strings. (LeetCode) Day-11

---
+++ Shreyam/Leetcode/Question1400.java
@@ -0,0 +1,37 @@
+// Question:- Construct K Palindrome Strings... 
+// Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+
+// Example 1:
+// Input: s = "annabelle", k = 2
+// Output: true
+// Explanation: You can construct two palindromes using all characters in s.
+// Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+
+package Shreyam.Leetcode;
+
+public class Question1400 {
+    static boolean constructPalindrome(String s, int k){
+        if(k> s.length()) return false;
+        if(k == s.length()) return true;
+
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch-'a']++;
+        }
+
+        int oddFreqCount = 0;
+        for(int i =0; i<26; i++){
+            if(freq[i]%2 != 0){
+                oddFreqCount++;
+            }
+        }
+
+        return oddFreqCount <= k;
+    }
+    public static void main(String[] args) {
+        String s = "true";
+        int k = 4;
+        boolean result = constructPalindrome(s, k);
+        System.out.println(result);
+    }
+}
