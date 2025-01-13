8ee18975472b7132b59a21e45034f45e735eda89
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-01-13 17:10:32 +0530

    Searching Challenge...

---
+++ Shreyam/TrickyQuestion/searchingChallenge.java
@@ -0,0 +1,33 @@
+// Question: Searching Challenge.. 
+// It takes the str parameter being passed and return 1 if the brackets are correctly matched and each one is accounted for. Otherwise return 0. If str contains no brackets return 0. Only "( and )" will be used as brackets.
+// Example: 
+// I/p: "(coder)(byte))"
+// O/p: 0
+
+// I/p: "(c(oder))b(yte)"
+// O/p: 1
+
+package Shreyam.TrickyQuestion;
+
+import java.util.Stack;
+
+public class searchingChallenge {
+    static String searchChallenge(String str){
+        Stack<Character> stack = new Stack<>();
+        for(char ch:str.toCharArray()){
+            if(ch == '('){
+                stack.push(ch);
+            }else if(ch == ')'){
+                if(stack.empty()){
+                    return "0";
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty() ? "1":"0";
+    }
+    public static void main(String[] args) {
+        String s = "(c(oder))b(yte)";
+        System.out.println(searchChallenge(s));
+    }
+}
