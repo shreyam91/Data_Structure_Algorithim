6d0db0ad425c0ae6473cfce0da3d152037bf312a
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-01-17 16:35:17 +0530

    Find Duplicate Number in array...

---
+++ Shreyam/Sorting/CycleSort/DuplicateNumber.java
@@ -0,0 +1,34 @@
+// Question:- Find the Duplicate number in an Array... 
+package Shreyam.Sorting.CycleSort;
+
+public class DuplicateNumber {
+    static int findDuplicate(int[] arr){
+        int length = arr.length;
+        int i=0;
+
+        while(i < length){
+            if(arr[i] != i+1){
+                int correct = arr[i]-1;
+                if(arr[i] != arr[correct]){
+                    swap(arr,i,correct);
+                }else{
+                    return arr[i];
+                }
+            }else{
+                i++;
+            }
+        }
+
+        return -1;
+    }
+    static void swap (int [] arr, int first, int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,3,4,2,2};
+        int res = findDuplicate(arr);
+        System.out.println(res);
+    }
+}
