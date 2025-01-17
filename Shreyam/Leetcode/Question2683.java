8f1a673272867ac5a9ea88ac44af40269fbd4931
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-01-17 11:43:30 +0530

    Neighboring Bitwise XOR.(LeetCode) Day-17..

---
+++ Shreyam/Leetcode/Question2683.java
@@ -0,0 +1,33 @@
+// Question: Neighboring Bitwise XOR... 
+// A 0-indexed array derived with length n is derived by computing the bitwise XOR (⊕) of adjacent values in a binary array original of length n.
+// Specifically, for each index i in the range [0, n - 1]:
+// If i = n - 1, then derived[i] = original[i] ⊕ original[0].
+// Otherwise, derived[i] = original[i] ⊕ original[i + 1].
+// Given an array derived, your task is to determine whether there exists a valid binary array original that could have formed derived.
+// Return true if such an array exists or false otherwise.
+// A binary array is an array containing only 0's and 1's
+ 
+// Example 1:
+// Input: derived = [1,1,0]
+// Output: true
+// Explanation: A valid original array that gives derived is [0,1,0].
+// derived[0] = original[0] ⊕ original[1] = 0 ⊕ 1 = 1 
+// derived[1] = original[1] ⊕ original[2] = 1 ⊕ 0 = 1
+// derived[2] = original[2] ⊕ original[0] = 0 ⊕ 0 = 0
+
+package Shreyam.Leetcode;
+
+public class Question2683 {
+     static boolean validArray(int[] derived){
+        int res =0;
+        for(int ele: derived){
+            res ^= ele;
+        }
+        return res == 0;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,0};
+        boolean res = validArray(arr);
+        System.out.println(res);
+    }
+}
