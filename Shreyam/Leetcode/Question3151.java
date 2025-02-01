91b7f2253be4718bea4b8ebfa43d6e922e78ab58
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-01 16:26:55 +0530

    Special Array I..(LeetCode) Day-32

---
+++ Shreyam/Leetcode/Question3151.java
@@ -0,0 +1,29 @@
+// Question: Special Array I... 
+// An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+// You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+
+// Example 1:
+// Input: nums = [1]
+// Output: true
+// Explanation:
+// There is only one element. So the answer is true.
+
+package Shreyam.LeetCode;
+
+public class Question3151 {
+    public static boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; ++i) {
+            if ((nums[i] % 2) == (nums[i - 1] % 2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] num = {1};
+        boolean res = isArraySpecial(num);
+        System.out.println(res);
+
+    }
+}
