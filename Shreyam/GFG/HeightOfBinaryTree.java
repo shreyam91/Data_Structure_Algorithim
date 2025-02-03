9155a5ebc7086a9df58e6dd246752a7fa3592038
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-03 21:35:26 +0530

    Height of Binary Tree...(GFG) Day-34

---
+++ Shreyam/GFG/HeightOfBinaryTree.java
@@ -0,0 +1,56 @@
+// Question: Height of Binary Tree... 
+// Given a binary tree, find its height.
+// The height of a tree is defined as the number of edges on the longest path from the root to a leaf node. A leaf node is a node that does not have any children.
+
+// Input: root[] = [12, 8, 18, 5, 11] 
+// Output: 2
+// Explanation: One of the longest path from the root (node 12) goes through node 8 to node 5, which has 2 edges.
+
+package Shreyam.GFG;
+
+public class HeightOfBinaryTree {
+    static class Node
+    {
+    int data;
+    Node left, right;
+
+    Node(int item)
+        {
+        data = item;
+        left = right = null;
+        }
+    }
+    int height(Node node) {
+        if(node == null){
+            return -1;
+        }
+
+        return Math.max(height(node.left), height(node.right))+1;
+    }
+
+    Node insert(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+    public static void main(String[] args) {
+        HeightOfBinaryTree tree = new HeightOfBinaryTree();
+        Node root = null;
+
+        int arr[] = {12,8,18,5,11};
+        for (int i = 0; i < arr.length; i++) {
+            root = tree.insert(root, arr[i]);
+        }
+
+        System.out.println("Height of the tree: " + tree.height(root));
+    }
+}
