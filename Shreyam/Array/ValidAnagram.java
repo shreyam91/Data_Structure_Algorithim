ac201634fb7b70fabdefac562b51814ec64dd12c
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-12 14:50:31 +0530

    Valid Anagram...

---
+++ Shreyam/Array/ValidAnagram.java
@@ -0,0 +1,43 @@
+// Question: Valid naagram... 
+package shreyam.array;
+
+public class ValidAnagram {
+
+    // <!--- Brute Force Approach ---> TC: O(nlogn) SC: O(n)
+    // static boolean isAnagram(String s, String t){
+    //         if(s.length() != t.length()){
+    //             return false;
+    //         }
+            
+    //         char[] sArray = s.toCharArray();
+    //         char[] tArray = t.toCharArray();
+
+    //         Arrays.sort(sArray);
+    //         Arrays.sort(tArray);
+
+    //         return Arrays.equals(sArray, tArray);
+    // }
+    // <!--- Optimal Approach ---> TC: O(n), SC: O(1)
+
+    static boolean isAnagram(String s, String t){
+        if(s.length() != t.length()) return false;
+
+        int[] charCount = new int[256];
+
+        for(int i=0; i<s.length();i++){
+            charCount[s.charAt(i)]++;
+            charCount[t.charAt(i)]--;
+        }
+
+        for(int count: charCount){
+            if( count != 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        boolean res = isAnagram(s, t);
+        System.out.println(res);
+    }
+}
