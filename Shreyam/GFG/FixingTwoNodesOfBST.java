16f5394b7262ee9fd052cdca5281d93a53390938
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-14 21:38:22 +0530

    .

--- Shreyam/GFG/FixingTwoNodesOfBST.java
+++ Shreyam/GFG/FixingTwoNodesOfBST.java
@@ -12,53 +12,44 @@ public class FixingTwoNodesOfBST {
     static class Node {
         int data;
         Node left;
-        Node right;
-    
-        Node(int data) {
-            this.data = data;
-            left = null;
-            right = null;
-        }
+        Node right;    
     }
 
     static Node first;
     static Node middle;
-        static Node last;
-                static Node prev;
-                    private static void inorder(Node root){
-                        if(root == null) return;
-                        inorder(root.left);
-                
-                        if(prev != null && prev.data > root.data){
-                        if(first == null){
-                            first = prev;
-                            middle = root;
-                    }else{
-                        last = root;
+    static Node last;
+    static Node prev;
+
+    private void inorder(Node root) {
+        if (root == null) return;
+
+        inorder(root.left);
+
+        if (prev != null && prev.data > root.data) {
+            if (first == null) {
+                first = prev;
+                middle = root; 
+            } else {
+                last = root; 
             }
         }
-        prev = root;
+        prev = root; 
+
         inorder(root.right);
     }
-
-    static void correctBST(Node root) {
-        first = middle = last = null;
+    void correctBST(Node root) {
+         first = middle = last = null;
         inorder(root);
 
-        if(first != null && last != null){
+        if (first != null && last != null) {
             int temp = first.data;
             first.data = last.data;
             last.data = temp;
         }
-        else if(first != null && middle != null){
+        else if (first != null && middle != null) {
             int temp = first.data;
             first.data = middle.data;
             middle.data = temp;
         }
-    }
-
-    public static void main(String[] args) {
-        int[] root = {10,5,8,2,20};
-
     }
 }
