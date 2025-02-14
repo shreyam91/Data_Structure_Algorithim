8ceff7cb0e9dfa379abe57ed77a59cb2b7c78d24
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-02-14 13:53:38 +0530

    Fixing Two nodes of a BST...(GFG) day-45

---
+++ Shreyam/GFG/FixingTwoNodesOfBST.java
@@ -0,0 +1,64 @@
+// Question: Fixing Two nodes of a BST... 
+// Given the root of a Binary search tree(BST), where exactly two nodes were swapped by mistake. Your task is to fix (or correct) the BST by swapping them back. Do not change the structure of the tree.
+// Note: It is guaranteed that the given input will form BST, except for 2 nodes that will be wrong. All changes must be reflected in the original linked list.
+ 
+// Examples :
+// Input: root = [10, 5, 8, 2, 20]
+// Output: 1
+
+package shreyam.gfg;
+
+public class FixingTwoNodesOfBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+    
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    static Node first;
+    static Node middle;
+        static Node last;
+                static Node prev;
+                    private static void inorder(Node root){
+                        if(root == null) return;
+                        inorder(root.left);
+                
+                        if(prev != null && prev.data > root.data){
+                        if(first == null){
+                            first = prev;
+                            middle = root;
+                    }else{
+                        last = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+    }
+
+    static void correctBST(Node root) {
+        first = middle = last = null;
+        inorder(root);
+
+        if(first != null && last != null){
+            int temp = first.data;
+            first.data = last.data;
+            last.data = temp;
+        }
+        else if(first != null && middle != null){
+            int temp = first.data;
+            first.data = middle.data;
+            middle.data = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] root = {10,5,8,2,20};
+
+    }
+}
