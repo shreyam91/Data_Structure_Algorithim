05199d916b3ebb50e2a3984b57519e3b4b57b5bc
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-04-28 20:56:53 +0530

    Count Subarrays of Length Three With a Condition..

--- Shreyam/Leetcode/Question3392.java
+++ Shreyam/Leetcode/Question3392.java
@@ -1,4 +1,15 @@
-public int countSubarrays(int[] nums) {
+// Question: Count Subarrays of Length Three With a Condition.. 
+// Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.
+
+// Example 1:
+// Input: nums = [1,2,1,4,1]
+// Output: 1
+
+
+package shreyam.leetCode;
+
+public class Question3392{
+  public static int countSubarrays(int[] nums) {
     int ans = 0;
 
     for (int i = 1; i + 1 < nums.length; ++i)
@@ -7,3 +18,12 @@ public int countSubarrays(int[] nums) {
 
     return ans;
 }
+public static void main(String[] args) {
+    int [] nums = {1,2,1,4,1};
+    int res = countSubarrays(nums);
+    System.out.println(res);
+}
+}
+
+
+
