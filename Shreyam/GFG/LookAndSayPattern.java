3b52952cf61f9e949177f083d4fe296eaf1271cb
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-05-14 10:26:42 +0530

    Look and Say Pattern #Day-134

---
+++ Shreyam/GFG/LookAndSayPattern.java
@@ -0,0 +1,59 @@
+// Question: Look and Say Pattern
+
+// Given an integer n. Return the nth row of the following look-and-say pattern.
+// 1
+// 11
+// 21
+// 1211
+// 111221
+// Look-and-Say Pattern:
+
+// To generate a member of the sequence from the previous member, read off the digits of the previous member, counting the number of digits in groups of the same digit. For example:
+
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+// 1211 is read off as "one 1, one 2, then two 1s" or 111221.
+// 111221 is read off as "three 1s, two 2s, then one 1" or 312211.
+// Examples:
+
+// Input: n = 5
+// Output: 111221
+
+
+package shreyam.gfg;
+
+public class LookAndSayPattern {
+    public static String countAndSay(int n) {
+
+       if (n == 1) return "1";
+
+        String curr = "1";
+
+        for (int i = 2; i <= n; i++) {
+            StringBuilder next = new StringBuilder();
+            int cnt = 1;
+
+            for (int j = 1; j < curr.length(); j++) {
+
+               
+                if (curr.charAt(j) == curr.charAt(j - 1)) {
+                    cnt++;
+      
+                } else {
+                    next.append(cnt).append(curr.charAt(j - 1));
+                    cnt = 1;
+                }
+            }
+
+            next.append(cnt).append(curr.charAt(curr.length() - 1));
+            curr = next.toString();
+        }
+
+        return curr;       
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(countAndSay(n));
+    }
+}
