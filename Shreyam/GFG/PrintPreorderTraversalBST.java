872556abba7a807906645d56b39e35a1fac07a83
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-05-27 13:32:14 +0530

    Print leaf nodes from preorder traversal BST #147

---
+++ Shreyam/GFG/PrintPreorderTraversalBST.java
@@ -0,0 +1,46 @@
+// Question: Print leaf nodes from preorder traversal of BST 
+
+// Given a preorder traversal of a BST, find the leaf nodes of the tree without building the tree.
+
+// Examples:
+// Input: preorder[] = [5, 2, 10]
+// Output: [2, 10]
+
+package shreyam.gfg;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PrintPreorderTraversalBST {
+    public static ArrayList<Integer> leafNodes(int[] preorder) {
+        Stack<Integer> s = new Stack<>();
+        ArrayList<Integer> leaves = new ArrayList<>();
+        int n = preorder.length;
+
+        for (int i = 0, j = 1; j < n; i++, j++) {
+            boolean found = false;
+            if (preorder[i] > preorder[j]) {
+                s.push(preorder[i]);
+            } else {
+                while (!s.isEmpty()) {
+                    if (preorder[j] > s.peek()) {
+                        s.pop();
+                        found = true;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            if (found) {
+                leaves.add(preorder[i]);
+            }
+        }
+        leaves.add(preorder[n - 1]);
+
+        return leaves;
+    }
+    public static void main(String[] args) {
+       int[] preorder = {5, 2, 10};
+        System.out.println(leafNodes(preorder));
+    }
+}
