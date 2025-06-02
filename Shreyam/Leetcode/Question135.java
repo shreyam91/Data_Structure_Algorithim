c91144bcaa63650bb740e99e9f10ad4318df1e7a
Shreyam Kanaujiya <72690437+shreyam91@users.noreply.github.com>
2025-06-02 18:35:02 +0530

    Candy #Day-153

---
+++ Shreyam/Leetcode/Question135.java
@@ -0,0 +1,47 @@
+// Question: Candy 
+
+// There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+// You are giving candies to these children subjected to the following requirements:
+// Each child must have at least one candy.
+// Children with a higher rating get more candies than their neighbors.
+// Return the minimum number of candies you need to have to distribute the candies to the children.
+
+// Example 1:
+// Input: ratings = [1,0,2]
+// Output: 5
+
+package shreyam.leetcode;
+
+import java.util.Arrays;
+
+public class Question135{
+    public static int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1]){
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+
+        for(int candy : candies){
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+    public static void main(String[] args) {
+        int [] ratings = {1,0,2};
+        System.out.println(candy(ratings));
+    }
+}
