fab6a46d5e4a530d5b65905895a80513fa7343b7
Shreyam <72690437+shreyam91@users.noreply.github.com>
2025-07-03 12:21:18 +0530

    Find the K-th Character in String Game I #Day-184

---
+++ Shreyam/Leetcode/Question3304.java
@@ -0,0 +1,25 @@
+// Question: Find the K-th Character in String Game I
+
+// Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+// You are given a positive integer k.
+// Now Bob will ask Alice to perform the following operation forever:
+// Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+// For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+// Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+// Note that the character 'z' can be changed to 'a' in the operation.
+
+// Example 1:
+// Input: k = 5
+// Output: "b"
+
+package shreyam.leetCode;
+
+public class Question3304 {
+    public static char kthCharacter(int k) {
+        return (char) ('a' + Integer.bitCount(k - 1));
+    }
+    public static void main(String[] args) {
+        int k =5;
+        System.out.println(kthCharacter(k));
+    }
+}
